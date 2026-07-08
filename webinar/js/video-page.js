5491067f452aac414eef59f5f757160b0e622b68
rezlucas <cmp.luccas@gmail.com>
2026-07-08 12:58:30 -0300

    persiste utms do formulario ate o link de cadastro na plataforma
    As utms ja eram salvas no sessionStorage pelo form do webinar; agora a
    video.html le esse valor e anexa como query string no link para
    app.foozi.com.br, que e um dominio externo e nao compartilha sessionStorage.

--- webinar/js/video-page.js
+++ webinar/js/video-page.js
@@ -6,4 +6,27 @@
   if (greeting && firstname) {
     greeting.textContent = ', ' + firstname;
   }
+
+  function getUtms() {
+    var keys = ['utm_source', 'utm_medium', 'utm_campaign', 'utm_content', 'utm_term'];
+    var utms = {};
+
+    keys.forEach(function (key) {
+      var fromUrl = params.get(key);
+      if (fromUrl) sessionStorage.setItem(key, fromUrl);
+      utms[key] = fromUrl || sessionStorage.getItem(key) || '';
+    });
+
+    return utms;
+  }
+
+  var platformLink = document.querySelector('a[href^="https://app.foozi.com.br"]');
+  if (platformLink) {
+    var utms = getUtms();
+    var url = new URL(platformLink.href);
+    Object.keys(utms).forEach(function (key) {
+      if (utms[key]) url.searchParams.set(key, utms[key]);
+    });
+    platformLink.href = url.toString();
+  }
 })();
